e87a6164ced0cdde92acf3ae904108482780df99
SUMAN MAJI <118503667+suman-maji@users.noreply.github.com>
2024-07-29 17:57:46 +0530

    Update Footer.jsx

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
   return (
     <div className='px-[5%] bg-gradient-to-b from-black to-gray-800  text-white'>
       <div className='pt-10 flex justify-between'>
-        <h1 className='text-xl sm:text-3xl font-semibold '>Snap-Study</h1>
+        <h1 className='text-xl sm:text-3xl font-semibold '>MAK-Book</h1>
         <NavLink to={"/"}>
-          <img src={logo} alt="snap-study" className=' rounded-md h-[40px] sm:h-[50px] cursor-pointer' />
+          <img src={logo} alt="makaut-book" className=' rounded-md h-[40px] sm:h-[50px] cursor-pointer' />
         </NavLink>
       </div>
 
@@ -20,7 +20,6 @@ const Footer = () => {
           <NavLink to={"/about"}>About</NavLink>
         </div>
         <div className='flex flex-col gap-2 '>
-          <a href="fb">Facebook</a>
           <a href="fb">LinkedIn</a>
           <a href="fb">Twitter</a>
         </div>
@@ -37,14 +36,14 @@ const Footer = () => {
       <br />
       <br /><br />
       <div className='text-2xl md:text-6xl flex gap-2 items-center' >
-        <a href='https://snapstudy-makaut.vercel.app/' className='hover:text-gray-400'>snapstudy-makaut</a>
+        <a href='https://makbook-makaut.vercel.app/' className='hover:text-gray-400'>makbook-makaut</a>
         <div className='text-4xl md:text-8xl'>
           <GoArrowDownRight/>
         </div>    
       </div>
 
       <hr />
-      <p className='my-3 text-xs sm:text-lg'>​© 2024 by Snap-Study. Created by <a className='text-red-600' href="https://github.com/PankajKumar1947">Pankaj Kumar</a> .</p>
+      <p className='my-3 text-xs sm:text-lg'>​© 2024 by Snap-Study. Created by <a className='text-red-600' href="https://github.com/suman-maji">Suman Maji</a> .</p>
       <hr />
     </div>
   )
